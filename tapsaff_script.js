16ffdabc3da8673d87aeed2e9dd41ccb0bb3d730
unknown <dhrisher@hotmail.co.uk>
2022-03-06 15:42:34 +0000

    add shirt svg and move temp info

--- tapsaff_script.js
+++ tapsaff_script.js
@@ -1,11 +1,15 @@
 let area = "edinburgh";
-const heading = document.querySelector(".heading");
+const temp = document.querySelector(".temperature");
 const tapsRating = document.querySelector(".taps_rating");
+const weatherText = document.querySelector(".weatherText");
+const weatherIcon = document.querySelector(".weatherIcon");
 
 fetch(`https:/\/api.weatherapi.com/v1/forecast.json?key=03a7522f574442939ee153422222602&q=${area}&days=7&aqi=no&alerts=no`)
   .then(response => response.json())
   .then(data => {console.log(data);
-                heading.innerHTML = `<h1>Current temp (${area}) = ${getCurrentTemp(data)} degrees centigrade</h1>`;
+                weatherText.innerHTML = `<span>${getCurrentWeatherText(data)}</span>`;
+                weatherIcon.innerHTML = `<img src="${getCurrentWeatherIcon(data)}" alt="Weather Icon">`;
+                temp.innerHTML = `<span>${getCurrentTemp(data)}℃</span>`;
                 tapsRating.style.height = getTopRating(data);
                 }
        );
@@ -16,8 +20,12 @@ function getCurrentTemp(data){
 
 
 
-function getCurrentWeather(data){
-  
+function getCurrentWeatherText(data){
+  return data.current.condition.text;
+}
+
+function getCurrentWeatherIcon(data){
+  return data.current.condition.icon;
 }
 
 function getTopRating(data){
